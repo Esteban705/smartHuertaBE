ce6d1aef253d37dbc6c30b07c9f1ce3dbaab1177
Ruben Esteban <esteban9aten@gmail.com>
2022-12-04 15:19:30 -0300

    correcciones en el codigo

--- routes/homes.js
+++ routes/homes.js
@@ -4,6 +4,7 @@
 */
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { crearCasa } = require('../controllers/homes');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
